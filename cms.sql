c56c27658e1e396a8a83d6f33b67a4fea64cb0dd
SJS <sjsevadjian@gmail.com>
2020-10-07 22:34:11 -0700

    initializing repo

---
+++ cms.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS cmsDB;
+
+CREATE DATABASE cmsDB;
+
+USE cmsDB;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE roles(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id)
+);
